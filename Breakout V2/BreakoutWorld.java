2874698ee56cdd1f3faf29bba8b262f73cb3b1b2
AyrtonEs <ay.rton11@hotmail.com>
2016-10-24 21:24:27 -0500

    Correccion de resolucion
    Se corrigio un erro en la resolucion que no permitia que se visualizara
    correctamente el juego

--- Breakout V2/BreakoutWorld.java
+++ Breakout V2/BreakoutWorld.java
@@ -26,11 +26,11 @@ public class BreakoutWorld extends World
    /** the distance between bricks in pixels (cells) */
    public static final int BRICK_SEP = 4;   
    /** the number of bricks per row */
-   public static final int NUM_BRICKS_PER_ROW = 2; 
+   public static final int NUM_BRICKS_PER_ROW = 10; 
    /** distance from the top edge in pixels (cells) */
    public static final int BRICK_Y_OFFSET = 50;     
    /** the number of pixels per cell */
-   public static final int RESOLUTION = 10;          
+   public static final int RESOLUTION = 1;          
    /** world width in pixels (cells) */
    public static final int WIDTH = (BRICK_WIDTH + BRICK_SEP) * NUM_BRICKS_PER_ROW + BRICK_SEP; 
    /** world height in pixels (cells) */
@@ -137,7 +137,7 @@ public class BreakoutWorld extends World
         contVidas.add(-1);
         if(contVidas.getValue()==0)
         {
-            Label etiquetaFin = new Label("Game Over",BreakoutWorld.WIDTH/4);
+            Label etiquetaFin = new Label("Game Over",BreakoutWorld.WIDTH/5);
             addObject(etiquetaFin,BreakoutWorld.WIDTH/2,BreakoutWorld.HEIGHT/2);
             Greenfoot.stop();
         }
